f62eeff4434a6914f084f21b14a59b82a8a74c59
Mike Air <mikeair@Mike-Air.local>
2017-12-29 16:35:39 -0800

    vantiqData

---
+++ vantiqData/vantiqueData.java
@@ -0,0 +1,11 @@
+package vantiqData;
+
+public class vantiqueData {
+	
+   public String findRecordType () {
+	   final String findRecordDropDownList ="//section[@id='pagecontent']/div/div/table/tbody/tr/td[2]/select";
+	   return findRecordDropDownList;
+	   
+	  }
+
+}
